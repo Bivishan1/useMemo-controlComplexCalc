eed239456b42b3fb5c8df8c4c63a63f943514147
Bivishan1 <sapkotabivishan@gmail.com>
2024-09-18 15:56:34 +0545

    calculating complex calcu in seperate state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [task, setTask] = useState([]);
+  const [count, setCount] = useState(0);
+  const calculation = useMemo(() => { expensiveCalculation(count); }, [count]);
+  function newCount() {
+    setCount((t) => t + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Add New Task</h1>
+        {task.map((task, index) => {
+          return (<p>{task}</p>)
+        })}
+        {/* new spreading operator with setting new array and their tasks,,,, */}
+        <button onClick={() => { setTask((t) => [...t, "new task"]) }}>Add Task +</button>
+      </div>
+      <div>
+        <h2>Add count</h2>
+        Count : {count}
+        <button onClick={newCount}>Add count</button>
+        {calculation}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//calculating expensive calculation at a same time.
+function expensiveCalculation(num) {
+  console.log('calculating....');
+  for (let i = 0; i < 1000000; i++) {
+    num += i;
+  }
+  return num;
+
+}
